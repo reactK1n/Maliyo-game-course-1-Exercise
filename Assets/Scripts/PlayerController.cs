822d3c52f73e29e23c3ba1d22d50d2019f6a97cc
reactK1n <adewings1995@gmail.com>
2023-09-15 15:57:14 -0700

    change-the git files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20.0f;
+	public float turnSpeed = 45.0f;
+    public float horizontalInput;
+	public float forwardInput;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+		forwardInput = Input.GetAxis("Vertical");
+
+		//we'll move forward
+		transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //moving to sides
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+}
+}
